b44a3bb699e754c907ebac6a342df501d3e2ba8d
draxyka <bader.laszlo22@gmail.com>
2025-01-27 12:32:32 +0100

    hotfix

--- src/app/wuthering-waves/page.tsx
+++ src/app/wuthering-waves/page.tsx
@@ -1,4 +1,4 @@
-import Core from '../core/core';
+import Core from '../../core/core';
 
 export default function WutheringWaves() {
   return <Core data={{ title: 'Wuthering Waves', type: 'wuwa' }} />;
