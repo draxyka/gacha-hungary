7668cd4a757aaafb66307e9f84501594b8fc9d57
draxyka <bader.laszlo22@gmail.com>
2026-02-07 15:52:07 +0100

    yt fix

--- src/features/social-media/social-media.mock.ts
+++ src/features/social-media/social-media.mock.ts
@@ -5,7 +5,7 @@ export const CHANNELS_DATA: Record<string, SocialMediaChannel[]> = {
   'wuthering-waves': [
     { id: '1', channelId: 'drax18', name: 'Drax18', type: ChannelType.Twitch },
     { id: '2', channelId: 'seshihira', name: 'Seshihira', type: ChannelType.Twitch },
-    { id: '3', channelId: 'UCiMhD4jzUqG-IgPzUmmytRQ', name: 'Linus Tech Tips', type: ChannelType.Youtube },
+    { id: '3', channelId: 'bjNTSBYoVgFEy_UY8LL4pA', name: 'Drax18', type: ChannelType.Youtube },
     { id: '4', channelId: 'UC1M9U7vBaLZHqwqltmxDIbg', name: 'Seshihira', type: ChannelType.Youtube },
   ],
   // TODO: Többi játék csatornái
